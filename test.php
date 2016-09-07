e1f4175557a907c5a83739ec2e1bda9c522b69e7
moneycashweed <phpdominator@gmail.com>
2016-09-07 13:02:38 +0300

    ds

--- test.php
+++ test.php
@@ -3,25 +3,48 @@
 $access_token = '214473630:AAHx0uBoSJ5_SqYdk8kIWAS0KmjunAt1x_A';
 $api = 'https://api.telegram.org/bot' . $access_token;
 
+/**
+ * Задаём основные переменные.
+ */
 $output = json_decode(file_get_contents('php://input'), TRUE);
 $chat_id = $output['message']['chat']['id'];
 $first_name = $output['message']['chat']['first_name'];
 $message = $output['message']['text'];
 
+/**
+ * Emoji для лучшего визуального оформления.
+ */
+$emoji = array(
+    'preload' => json_decode('"\uD83D\uDE03"'), // Улыбочка.
+    'weather' => array(
+        'clear' => json_decode('"\u2600"'), // Солнце.
+        'clouds' => json_decode('"\u2601"'), // Облака.
+        'rain' => json_decode('"\u2614"'), // Дождь.
+        'snow' => json_decode('"\u2744"'), // Снег.
+    ),
+);
+
+/**
+ * Получаем команды от пользователя.
+ */
 switch($message) {
+    // API погоды предоставлено OpenWeatherMap.
+    // @see http://openweathermap.org
     case '/pogoda':
-        $preload_text = 'Одну секунду, ' . $first_name . ' ' . ' Я уточняю для вас погоду..';
+        // Отправляем приветственный текст.
+        $preload_text = 'Одну секунду, ' . $first_name . ' ' . $emoji['preload'] . ' Я уточняю для вас погоду..';
         sendMessage($chat_id, $preload_text);
-        $apikey= $access_token;
+        // API key для OpenWeatherMap.
+        $apikey= '214473630:AAHx0uBoSJ5_SqYdk8kIWAS0KmjunAt1x_A';
         // ID для города/района/местности (есть все города РФ).
-        $id = '500776';
+        $id = '500776'; // Для примера: Петербург, север города.
         // Получаем JSON-ответ от OpenWeatherMap.
         $pogoda = json_decode(file_get_contents('http://api.openweathermap.org/data/2.5/weather?appid=' . $apikey . '&id=' . $id . '&units=metric&lang=ru'), TRUE);
         // Определяем тип погоды из ответа и выводим соответствующий Emoji.
-        if ($pogoda['weather'][0]['main'] === 'Clear') { $pogoda['weather'][0]['description']; }
-        elseif ($pogoda['weather'][0]['main'] === 'Clouds') { $pogoda['weather'][0]['description']; }
-        elseif ($pogoda['weather'][0]['main'] === 'Rain') { $pogoda['weather'][0]['description']; }
-        elseif ($pogoda['weather'][0]['main'] === 'Snow') { $pogoda['weather'][0]['description']; }
+        if ($pogoda['weather'][0]['main'] === 'Clear') { $weather_type = $emoji['weather']['clear'] . ' ' . $pogoda['weather'][0]['description']; }
+        elseif ($pogoda['weather'][0]['main'] === 'Clouds') { $weather_type = $emoji['weather']['clouds'] . ' ' . $pogoda['weather'][0]['description']; }
+        elseif ($pogoda['weather'][0]['main'] === 'Rain') { $weather_type = $emoji['weather']['rain'] . ' ' . $pogoda['weather'][0]['description']; }
+        elseif ($pogoda['weather'][0]['main'] === 'Snow') { $weather_type = $emoji['weather']['snow'] . ' ' . $pogoda['weather'][0]['description']; }
         else $weather_type = $pogoda['weather'][0]['description'];
         // Температура воздуха.
         if ($pogoda['main']['temp'] > 0) { $temperature = '+' . sprintf("%d", $pogoda['main']['temp']); }
@@ -35,12 +58,21 @@ switch($message) {
         elseif ($pogoda['wind']['deg'] > 191.25 && $pogoda['wind']['deg'] <= 258.75) { $wind_direction = 'юго-западный, '; }
         elseif ($pogoda['wind']['deg'] > 258.75 && $pogoda['wind']['deg'] <= 281.25) { $wind_direction = 'западный, '; }
         elseif ($pogoda['wind']['deg'] > 281.25 && $pogoda['wind']['deg'] <= 348.75) { $wind_direction = 'северо-западный, '; }
-        else { $wind_direction = 'yhhyyhyththyhth '; }
-        $weather_text = 'Сейчас ' . '. Температура воздуха: ' . $temperature . '°C. Ветер ' . $wind_direction . sprintf("%u", $pogoda['wind']['speed']) . ' м/сек.';
+        else { $wind_direction = ' '; }
+        // Формирование ответа.
+        $weather_text = 'Сейчас ' . $weather_type . '. Температура воздуха: ' . $temperature . '°C. Ветер ' . $wind_direction . sprintf("%u", $pogoda['wind']['speed']) . ' м/сек.';
         // Отправка ответа пользователю Telegram.
         sendMessage($chat_id, $weather_text);
         break;
     default:
         break;
+}
+
+/**
+ * Функция отправки сообщения sendMessage().
+ */
+function sendMessage($chat_id, $message) {
+    file_get_contents($GLOBALS['api'] . '/sendMessage?chat_id=' . $chat_id . '&text=' . urlencode($message));
+}
 }
 ?>
